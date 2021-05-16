26533769173e455664713d5ec583329b744b4b7e
A K <ameya.2000k@gmail.com>
2021-05-16 15:14:06 +0530

    Fifth Question

---
+++ Array Questions/K th SmallestLargest Element in Unsorted Array  Set 1/Code.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int kSmall(int arr[], int n, int k){
+    for(int i = 0; i<n; i++){
+        for(int j = 0;j<n-i-1; j++){
+            if(arr[j]>arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    cout<<arr[k-1];
+    return 0;
+}
+
+int main(){
+    int arr[] = {7, 10, 4, 3, 20, 15};
+    int k = 4;
+    int n = sizeof(arr)/sizeof(arr[0]);
+    kSmall(arr,n,k);
+}
