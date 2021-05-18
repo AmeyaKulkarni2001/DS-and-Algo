26c1a35a5717f06c303383dd11ee56f8eff93be9
A K <ameya.2000k@gmail.com>
2021-05-18 12:39:18 +0530

    Question Ten

--- Array Questions/Union and Intersection of two sorted arrays/Code.cpp
+++ Array Questions/Union and Intersection of two sorted arrays/Code.cpp
@@ -18,18 +18,7 @@ void arrUnion(int arr1[], int arr2[],int n1, int n2){
         arr3[i+j+1] = arr2[j];
     }
     sort(arr3,arr3+n1+n2);
-    // for(int j = 0;j<n1+n2; j++){
-    //     if(arr3[j] == arr3[j+1]){
-    //         arr3[j+1] = arr3[j+2];
-    //     }
-    //     arr3[n1+n2-1] = {};
-    // }
-    // for(int j = n1+n2;j>=0; j--){
-    //     if(arr3[j-1] == arr3[j]){
-    //         arr3[j-1] = arr3[j-2];
-    //     }
-    //     arr3[n1+n2-1] = {};
-    // }
+    
     for(int j = 0; j<n1 + n2; j++){
         if(arr3[j] == arr3[j+1]) {
             for(int k=j+2; k<n1+n2; k++) {
@@ -40,10 +29,27 @@ void arrUnion(int arr1[], int arr2[],int n1, int n2){
     }
     arrDisplay(arr3,n1+n2);
 }
+
+void arrInter(int arr1[],int arr2[],int n1,int n2){
+    int x = 0;
+    int arr3[x];
+
+    for(int i = 0;i<n1;i++){
+        for(int j = 0;j<n2;j++){
+            if(arr1[i] == arr2[j]){
+                arr3[x] = arr2[j];
+                x++;
+            }
+        }
+    }
+    arrDisplay(arr3,x);
+}
+
 int main(){
     int arr1[] = {1, 3, 4, 5, 7};
     int arr2[] = {2, 3, 5, 6};
     int n1 = sizeof(arr1)/sizeof(arr1[0]);
     int n2 = sizeof(arr2)/sizeof(arr2[0]);
     arrUnion(arr1,arr2,n1,n2);
+    arrInter(arr1,arr2,n1,n2);
 }
