3e0444c37a18c8521219d2c98e6ae42a0edf69ac
A K <ameya.2000k@gmail.com>
2021-05-19 19:39:22 +0530

    Question 18 update

--- Array Questions/Non-Repeating Element/Code.cpp
+++ Array Questions/Non-Repeating Element/Code.cpp
@@ -2,22 +2,19 @@
 using namespace std;
 
 void arrNonRep(int arr[],int n){
-    for(int i =0;i<n;i++){
+    for(int i = 0; i<n; i++){
         int flag = 0;
-        for(int j=0;j<n;j++){
-            if(j != i){
-                if(arr[i] == arr[j]){
-                    flag =1;
-                    break;
-                }
-            }  
+        for(int j = 0;j<n;j++){
+            if(j!=i && arr[i] == arr[j]){
+                flag = 1;
+                break;
+            } 
         }
         if(flag == 0){
-            cout<<"\n"<<arr[i]<<" Is the first non repeating int";
+            cout<<arr[i]<<" Is the first non repeating number";
             break;
-        } else{
-            cout<<"\n"<<flag<<" Flag \n";
-        } 
+        }
+
     }
 }
 
