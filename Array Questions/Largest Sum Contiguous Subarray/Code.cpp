09199f3c5cf529576a7a633648a15bd85230ae38
A K <ameya.2000k@gmail.com>
2021-05-20 13:55:42 +0530

    Question 22

---
+++ Array Questions/Largest Sum Contiguous Subarray/Code.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void arrConti(int arr[],int n){
+    int curr, max = 0;
+    for(int i = 0; i<n;i++){
+        curr = arr[i];
+        for(int j = i+1; j<n;j++){
+            curr += arr[j];
+            if(curr>max){
+                max = curr;
+            }
+        }
+    }
+    cout <<max;
+}
+
+int main(){
+    int arr[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    arrConti(arr,n);
+    return 0;
+}
