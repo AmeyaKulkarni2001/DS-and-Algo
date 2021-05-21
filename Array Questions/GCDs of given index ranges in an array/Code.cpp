b1b0264d880f1b7675e8b45383d7723eb8ca4d16
A K <ameya.2000k@gmail.com>
2021-05-21 15:03:24 +0530

    Question 28(Incomplete)

---
+++ Array Questions/GCDs of given index ranges in an array/Code.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void arrGCD(int arr[],int n,int qs,int qe){
+    for(int i = qs; i<=qe; i++){
+        /* function to write gcd*/
+    }
+}
+
+int main(){
+    int arr[] = {};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int qs,qe;
+
+    arrGCD(arr,n,qs,qe);
+}
+
