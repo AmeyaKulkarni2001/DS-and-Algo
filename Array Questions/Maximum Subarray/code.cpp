e16a979640e69b950e217d070da4b7f708085f52
A K <ameya.2000k@gmail.com>
2021-09-15 22:38:32 +0530

    Maximum Sum Kadane's Algorithm

--- Array Questions/Maximum Subarray/code.cpp
+++ Array Questions/Maximum Subarray/code.cpp
@@ -7,13 +7,14 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int maxsum = -100000;
-        for(int i = 0; i<nums.size(); i++){
-            int sum = 0;
-            for(int j = i; j<nums.size(); j++){
-                sum += nums[j];
-                if(sum > maxsum){
-                    maxsum = sum;
-                }
+        int curr = 0;
+        for(int i = 0; i < nums.size(); i++){
+            curr += nums[i];
+            if(curr > maxsum){
+                maxsum = curr;
+            }
+            if(curr < 0){
+                curr = 0;
             }
         }
         return maxsum;
